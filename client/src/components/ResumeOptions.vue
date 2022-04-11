76c3e6fe60ed38fa81b1738d4571518f93491100
chriscorchado125 <chris.corchado125@gmail.com>
2022-04-10 22:04:56 -0400

    works

--- client/src/components/ResumeOptions.vue
+++ client/src/components/ResumeOptions.vue
@@ -3,25 +3,25 @@
   div#profiles
 
     div#pdf-resume.icon
-      a(href="https://chriscorchado.com/resume/Chris-Corchado.pdf" target="_blank" rel="noopener" title="Opening a new window")
+      a(href="https://chriscorchado.com/resume/Chris-Corchado-resume-2020.pdf" target="_blank" rel="noopener" title="Opening a new window")
         img(alt="Link to PDF Resume" src="https://chriscorchado.com/images/pdfIcon.jpg")
         span PDF
 
     div#word-resume.icon
-      a(href="https://chriscorchado.com/resume/Chris-Corchado.docx" title="File will download")
+      a(href="https://chriscorchado.com/resume/Chris-Corchado-resume-2020.docx" title="File will download")
         img(alt="Link to MS Word Resume" src="https://chriscorchado.com/images/wordIcon.jpg")
         span Word
 
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import Component from 'vue-class-component';
+import Vue from "vue";
+import Component from "vue-class-component";
 
 @Component
 export default class Profile extends Vue {}
 </script>
 
 <style scoped lang="scss">
-@import './../scss/header/profile.scss';
+@import "./../scss/header/profile.scss";
 </style>
